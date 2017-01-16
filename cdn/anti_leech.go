f311f3f13509e4b8349a73c0d053e44ab2495edb
TianZong48 <tianzong48@gmail.com>
2017-01-16 18:46:59 +0800

    anti leech refactor

--- cdn/anti_leech.go
+++ cdn/anti_leech.go
@@ -11,7 +11,7 @@ import (
 // 构建带时间戳防盗链的链接
 // host需要加上 "http://" 或 "https://"
 // encryptKey 七牛防盗链key
-func CreateTimestampAntiLeechUrl(host, fileName string, queryStr url.Values, encryptKey string, durationInSeconds int64) (string, error) {
+func CreateTimestampAntiLeechUrl(host, fileName string, queryStr url.Values, encryptKey string, durationInSeconds int64) (antiLeechUrl string, err error) {
 
 	var urlStr string
 	if queryStr != nil {
@@ -22,15 +22,11 @@ func CreateTimestampAntiLeechUrl(host, fileName string, queryStr url.Values, enc
 
 	u, parseErr := url.Parse(urlStr)
 	if parseErr != nil {
-		return "", parseErr
+		err = parseErr
+		return
 	}
-	return createTimestampAntiLeechUrl(u, encryptKey, durationInSeconds), nil
 
-}
-
-func createTimestampAntiLeechUrl(u *url.URL, encryptKey string, duration int64) string {
-
-	expireTime := time.Now().Add(time.Second * time.Duration(duration)).Unix()
+	expireTime := time.Now().Add(time.Second * time.Duration(durationInSeconds)).Unix()
 	toSignStr := fmt.Sprintf("%s%s%x", encryptKey, u.EscapedPath(), expireTime)
 	signedStr := fmt.Sprintf("%x", md5.Sum([]byte(toSignStr)))
 
@@ -39,6 +35,8 @@ func createTimestampAntiLeechUrl(u *url.URL, encryptKey string, duration int64) 
 	q.Add("t", fmt.Sprintf("%x", expireTime))
 	u.RawQuery = q.Encode()
 
-	return fmt.Sprintf("%s://%s%s?%s", u.Scheme, u.Host, u.Path, u.Query().Encode())
+	antiLeechUrl = fmt.Sprintf("%s://%s%s?%s", u.Scheme, u.Host, u.EscapedPath(), u.Query().Encode())
+
+	return
 
 }
