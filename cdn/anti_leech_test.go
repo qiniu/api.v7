34c1507d225a568b76aad46bf7b526bf259e30e0
TianZong48 <tianzong48@gmail.com>
2017-01-16 18:49:26 +0800

    remove useless test

--- cdn/anti_leech_test.go
+++ cdn/anti_leech_test.go
@@ -42,25 +42,3 @@ func TestCreateTimestampAntiLeech(t *testing.T) {
 		})
 	}
 }
-
-func Test_createTimestampAntiLeechUrl(t *testing.T) {
-	type args struct {
-		u          *url.URL
-		encryptKey string
-		duration   int64
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := createTimestampAntiLeechUrl(tt.args.u, tt.args.encryptKey, tt.args.duration); got != tt.want {
-				t.Errorf("createTimestampAntiLeechUrl() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
