054716c92f6b45d46c980d415e4ba6ff91f0e80b
TianZong48 <tianzong48@gmail.com>
2017-06-30 15:51:29 +0800

    update doc.go of package `cdn`

--- cdn/doc.go
+++ cdn/doc.go
@@ -8,14 +8,14 @@
 
 比如我们要生成一个带时间戳防盗链的链接：
 	q :=url.Values{}// url.Values 请求参数
-	link, err := cdn.CreateTimestampAntiLeechUrl("http://www.qiniu.com", "abc/bcc/aa-s.mp4", nil, "encryptedkey", 20)
+	link, err := cdn.CreateTimestampAntileechURL(""http://www.qiniu.com/abc/bcc/aa-s.mp4?x=2&y=3", "encryptedkey", 20)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(link)
 
 又或者我们要列出CDN日志及其下载地址：
-	resp, err := cdn.GetLogOfDomain("2016-12-26", "x-mas.com")
+	resp, err := cdn.GetCdnLogList("2016-12-26", "x-mas.com")
 	if err != nil {
 		fmt.Println(err)
 	}
