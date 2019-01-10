64ec19f8fd3a11dcc99124e767c13a812ca57ea3
蔡加强 <tonycai653@yahoo.com>
2019-01-10 18:02:35 +0800

    qbox_test

--- auth/qbox/qbox_test.go
+++ auth/qbox/qbox_test.go
@@ -1,7 +1,6 @@
 package qbox
 
 import (
-	"net/http"
 	"testing"
 )
 
@@ -16,12 +15,11 @@ func TestMacSign(t *testing.T) {
 		Data   string
 		Signed string
 	}{
-		{Data: "hello", Signed: "ak:MzQzMzdjNzBjZDJiYzI4YjMxODQ3MjdhNDAwNzA4ZWUyNmE1YWY0OA=="},
-		{Data: "world", Signed: "ak:YzE5ZmFjYzM4ZDVhY2ExZGNmMTQzOTkwMDNlMGY3YTNiNzgxMjQ4Ng=="},
-		{Data: "-test", Signed: "ak:YTA5ZTdkYjE5NmFjODk2NGFhMmY1YTNiYmEwNjZjZTRlMjI3MTBhZQ=="},
-		{Data: "ba#a-", Signed: "ak:YjZhMWNiZjE1ZDgxNmNkMjM0NzU1MGQ3YjJmYjllNjY5ZmY2NDI3Mg=="},
+		{Data: "hello", Signed: "ak:NDN8cM0rwosxhHJ6QAcI7ialr0g="},
+		{Data: "world", Signed: "ak:wZ-sw41ayh3PFDmQA-D3o7eBJIY="},
+		{Data: "-test", Signed: "ak:oJ59sZasiWSqL1o7ugZs5OInEK4="},
+		{Data: "ba#a-", Signed: "ak:tqHL8V2BbNI0dVDXsvueZp_2QnI="},
 	}
-
 	for _, b := range testStrs {
 		got := mac.Sign([]byte(b.Data))
 		if got != b.Signed {
