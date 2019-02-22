ef9bd29291f8a3306b9ddfb81a3fbdd025326697
蔡加强 <tonycai653@yahoo.com>
2019-02-22 10:36:47 +0800

    调试UpdateObjectStatus

--- storage/bucket_test.go
+++ storage/bucket_test.go
@@ -88,11 +88,11 @@ func TestUpdateObjectStatus(t *testing.T) {
 	keysToStat := []string{"qiniu.png"}
 
 	for _, eachKey := range keysToStat {
-		err := objManager.UpdateObjectStatus(testBucket, eachKey, true)
+		err = objManager.UpdateObjectStatus(testBucket, eachKey, false)
 		if err != nil {
 			t.Fatalf("UpdateObjectStatus error: %v\n", err)
 		}
-		err = objManager.UpdateObjectStatus(testBucket, eachKey, false)
+		err := objManager.UpdateObjectStatus(testBucket, eachKey, true)
 		if err != nil {
 			t.Fatalf("UpdateObjectStatus error: %v\n", err)
 		}
