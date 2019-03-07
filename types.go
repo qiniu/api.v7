46d3f4164d1491175b125ad4ea63829f24f9d9be
蔡加强 <tonycai653@yahoo.com>
2019-03-07 14:34:55 +0800

    remote unnecessary funcs

--- types.go
+++ types.go
@@ -6,22 +6,6 @@ import (
 	"net/http"
 )
 
-// Bool 获取一个bool类型的指针
-func Bool(v bool) *bool {
-	return &v
-}
-
-// String获取一个字符串指针
-func String(v string) *string {
-	return &v
-}
-
-// Int64 获取一个整形的指针
-func Int64(v int64) *int64 {
-	i := int64(v)
-	return &i
-}
-
 // BytesFromRequest 读取http.Request.Body的内容到slice中
 func BytesFromRequest(r *http.Request) (b []byte, err error) {
 	if r.ContentLength == 0 {
