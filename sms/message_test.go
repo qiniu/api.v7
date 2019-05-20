74f6c0141584f7afa46f9af38e2888eaa9e4be3b
Miclle Zheng <miclle.zheng@gmail.com>
2019-05-20 12:00:19 +0800

    Disable SMS send message unit test [skip ci]

--- sms/message_test.go
+++ sms/message_test.go
@@ -1,30 +1,30 @@
 package sms_test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/qiniu/api.v7/sms"
-)
+// 	"github.com/qiniu/api.v7/sms"
+// )
 
-func TestMessage(t *testing.T) {
+// func TestMessage(t *testing.T) {
 
-	// SendMessage
-	args := sms.MessagesRequest{
-		SignatureID: "",
-		TemplateID:  "",
-		Mobiles:     []string{""},
-		Parameters: map[string]interface{}{
-			"code": 123456,
-		},
-	}
+// 	// SendMessage
+// 	args := sms.MessagesRequest{
+// 		SignatureID: "",
+// 		TemplateID:  "",
+// 		Mobiles:     []string{""},
+// 		Parameters: map[string]interface{}{
+// 			"code": 123456,
+// 		},
+// 	}
 
-	ret, err := manager.SendMessage(args)
+// 	ret, err := manager.SendMessage(args)
 
-	if err != nil {
-		t.Fatalf("SendMessage() error: %v\n", err)
-	}
+// 	if err != nil {
+// 		t.Fatalf("SendMessage() error: %v\n", err)
+// 	}
 
-	if len(ret.JobID) == 0 {
-		t.Fatal("SendMessage() error: The job id cannot be empty")
-	}
-}
+// 	if len(ret.JobID) == 0 {
+// 		t.Fatal("SendMessage() error: The job id cannot be empty")
+// 	}
+// }
