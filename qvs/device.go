f4103c79fd47d7078d84dd67325e4dfb0bd6da1f
zhaolei <zhaolei@qiniu.com>
2020-12-07 17:28:52 +0800

    add ptz

--- qvs/device.go
+++ qvs/device.go
@@ -55,6 +55,11 @@ type deviceVideoItem struct {
 	Type     string        `json:"type"`
 }
 
+type Preset struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 /*
    创建设备API
    参数device需要赋值字段:
@@ -193,4 +198,67 @@ func (manager *Manager) QueryGBRecordHistories(nsId, gbId, chId string, start, e
 		return nil, err
 	}
 	return &ret, nil
+}
+
+/*
+    用于对摄像头进行 转动镜头，如水平、垂直、缩放等操作
+	cmd: left(向左), right(向右), up(向上), down(向下), leftup(左上), rightup(右上), leftdown(左下), rightdown(右下), zoomin(放大), zoomout(缩小),stop(停止PTZ操作)
+	speed: 调节速度(1~10, 默认位5)
+*/
+func (manager *Manager) DevicePtz(nsId, gbId, chId string, cmd string, speed int) error {
+	req := M{"cmd": cmd, "speed": speed, "chId": chId}
+	return manager.client.CallWithJson(context.Background(), nil, "POST", manager.url("/namespaces/%s/devices/%s/ptz", nsId, gbId), nil, req)
+}
+
+/*
+	变焦控制
+	cmd: focusnear(焦距变近), focusfar(焦距变远),stop(停止)
+	speed: 调节速度(1~10, 默认位5)
+*/
+func (manager *Manager) DeviceFocus(nsId, gbId, chId string, cmd string, speed int) error {
+	req := M{"cmd": cmd, "speed": speed, "chId": chId}
+	return manager.client.CallWithJson(context.Background(), nil, "POST", manager.url("/namespaces/%s/devices/%s/focus", nsId, gbId), nil, req)
+}
+
+/*
+	用于摄像头的光圈控制
+	cmd: irisin(光圈变小), irisout(光圈变大),stop(停止)
+	speed: 调节速度(1~10, 默认位5)
+*/
+func (manager *Manager) DeviceIris(nsId, gbId, chId string, cmd string, speed int) error {
+	req := M{"cmd": cmd, "speed": speed, "chId": chId}
+	return manager.client.CallWithJson(context.Background(), nil, "POST", manager.url("/namespaces/%s/devices/%s/iris", nsId, gbId), nil, req)
+}
+
+/*
+	用于预置位的管理
+	cmd: set(新增预置位), goto(设置),remove(删除)
+	name: 预置位名称(cmd为set时有效,支持中文)
+	presetId: 预置位ID
+*/
+func (manager *Manager) DevicePresets(nsId, gbId, chId string, cmd, name string, presetId int) (*Preset, error) {
+	req := M{"cmd": cmd, "name": name, "presetId": presetId, "chId": chId}
+	var ret Preset
+	err := manager.client.CallWithJson(context.Background(), &ret, "POST", manager.url("/namespaces/%s/devices/%s/presets", nsId, gbId), nil, req)
+	if err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+/*
+	用于获取预置位列表
+	fetch: 是否强制从设备获取预置位
+*/
+func (manager *Manager) QueryDevicePresets(nsId, gbId, chId string, fetch bool) ([]Preset, error) {
+	ret := struct {
+		Items []Preset `json:"items"`
+	}{}
+
+	query := url.Values{}
+	setQuery(query, "chId", chId)
+	setQuery(query, "fetch", fetch)
+
+	err := manager.client.Call(context.Background(), &ret, "GET", manager.url("/namespaces/%s/devices/%s/presets?%v", nsId, gbId, query.Encode()), nil)
+	return ret.Items, err
 }
